93835f430ffc0e7a9001719fb699a03ab2ab2f88
Diego Baldeschi <diego.baldeschi@comm-it>
2019-07-06 18:11:57 +0200

    setup travis

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,15 @@ module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
   base: '/vuedoc/',
+  plugins: [
+    'vuepress-plugin-nprogress',
+    ['serve', {
+      port: 1234,
+      staticOptions: {
+        dotfiles: 'allow',
+      },
+    }],
+  ]
   // configureWebpack: {
   //   resolve: {
   //     alias: {
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,5 @@
 # Hello VuePress {{ $page }}
 
 ### Badge <Badge text="beta" type="warn"/> <Badge text="0.10.1+"/> <Badge text="default theme"/>
+
+### Published with Travis
